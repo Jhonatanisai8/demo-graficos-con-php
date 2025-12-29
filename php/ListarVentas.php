62a540efe2654835cb49ee9c97ac78293d64563d
jhonatansai <ojedasnachezjhonatan@gmail.com>
2025-12-28 21:42:49 -0500

    fichero para obtener los datos de la tabla ventas

---
+++ php/ListarVentas.php
@@ -0,0 +1,16 @@
+<?php
+require_once "./database/Conexion.php";
+header('Content-Type: application/json');
+try {
+    $conexion_SQL = conexion();
+    $query = "SELECT mes, monto FROM ventas ORDER BY id ASC";
+    $resultado = $conexion_SQL->query($query);
+    $datos = [];
+    foreach ($resultado as $fila) {
+        $datos[] = $fila;
+    }
+    echo json_encode($datos);
+    $conexion_SQL = null;
+} catch (\Throwable $th) {
+    echo "error al obtener datos: " . $th->getMessage();
+}
